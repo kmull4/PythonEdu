26ef862a064ceb1a7e25d601a1a385dd309bb492
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-06-09 19:39:49 -0400

    Completed u1p3, renamed u0p0

--- MITx 6.00.1x/u1p3 longest alphabetical.py
+++ MITx 6.00.1x/u1p3 longest alphabetical.py
@@ -16,12 +16,46 @@ Note: This problem may be challenging. We encourage you to work smart. If
  problem after you've had a break and cleared your head. 
 '''
 
-# s is assumed but here for the debug process
-s = 'azcbobobegghakl'
+# if this doesn't work, look at the enumerate function
 
-# tracker for the current letter
-index = ''
-# score for longest
-score = 0
+debug = True
+
+# s is assumed but here for the debug process
+if debug:
+    s = 'abcdefghijklmnopqrstuvwxyz'
+    print('len of s =', len(s))
+
+# set score, high score, and high score str
+score, high_score, high_score_str = 0, 0, s[0]
+# counter to keep track of position in loop
+counter = 0
+# position to keep track of ??
+pos = 0
 
 # parse through string and determine if subseqent letters are < str values
+for i in s:
+    if debug:
+        print('i =', i, 'counter =', counter, 'pos =', pos)
+    counter = pos
+    # while loop to keep proceeding to next letter and setting score
+
+    for j in range(len(s[pos:-1])):
+        if s[counter] <= s[counter+1]:
+            score += 1
+            counter += 1
+            if debug:
+                print('score =', score, 'counter =', counter)
+    # determine high score
+    if debug:
+        print(high_score)
+    if score > high_score:
+        high_score = score
+        # set high_score_str
+        high_score_str = s[pos:pos+score+1]
+
+    # reset score
+    score = 0
+    # position in string moves
+    pos += 1
+
+print('Longest substring in alphabetical order is:', high_score_str)
