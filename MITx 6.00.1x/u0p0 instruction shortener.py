5a3f70fb2019e70253bff59bf062c022b509ab6f
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-06-17 20:47:55 -0400

    completed more assignments, thought some were fun
    or good learning experiences. I left comments to myself in case I ever read them again some day.

--- MITx 6.00.1x/u0p0 instruction shortener.py
+++ MITx 6.00.1x/u0p0 instruction shortener.py
@@ -25,7 +25,16 @@ gcd(9, 12) = 3
 
 gcd(17, 12) = 1
 
-Write an iterative function, gcdIter(a, b), that implements this idea. One easy way to do this is to begin with a test value equal to the smaller of the two input arguments, and iteratively reduce this test value by 1 until you either reach a case where the test divides both a and b without remainder, or you reach 1.
+A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+
+If b = 0, then the answer is a
+
+Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+
+See this website for an example of Euclid's algorithm being used to find the gcd.
+
+Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one integer.
+
 
 '''
 
