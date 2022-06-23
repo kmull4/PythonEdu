0bf631c92b546c52acc22fa259ca0bdb6160a029
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-06-22 20:47:58 -0400

    Update 00 instruction shortener.py

--- MITx 6.00.1x/00 instruction shortener.py
+++ MITx 6.00.1x/00 instruction shortener.py
@@ -16,19 +16,39 @@ Idea for v2: interact with clipboard or other .py files
 instructions = '''Note: In programming there are many ways to solve a problem.
 For example, some asled dsfgsdfg dfjasdfg jwfe oej fmay aljw43td f ljawo48jgf ad when a09s8df0 98098 but also owjaf23.
 
+<<<<<<< Updated upstream
 Understandably, 304dsfg sdfg 9 sdfg sdfg sdfssdfg dfg 8tjqg3sdfg 567df g3 scgbn saapjefg!!!!
 
 Oh man, it's so close.
 But I need to take a break and test this fork situation too.'''
+=======
+You'll notice that in Problem 2, your monthly payment had to be a multiple of $10. Why did we make it that way? You can try running your code locally so that the payment can be any dollar and cent amount (in other words, the monthly payment is a multiple of $0.01). Does your code still work? It should, but you may notice that your code runs more slowly, especially in cases with very large balances and interest rates. (Note: when your code is running on our servers, there are limits on the amount of computing time each submission is allowed, so your observations from running this experiment on the grading system might be limited to an error message complaining about too much time taken.)
+
+Well then, how can we calculate a more accurate fixed monthly payment than we did in Problem 2 without running into the problem of slow code? We can make this program run faster using a technique introduced in lecture - bisection search!
+
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+'''
+>>>>>>> Stashed changes
 
 
 ##############################################################
 debug = True
+<<<<<<< Updated upstream
 # max length of string before break.
 max_len = 79
 if debug:
     max_len = 20 # to make debugging easier
 # variables for later use
+=======
+# max length of string before break. 
+max_len = 78
+# I wonder if I even need enumerate if this tracker is used
+>>>>>>> Stashed changes
 new_line_tracker, last_space = 0, 0
 
 if debug: # a good starting point
