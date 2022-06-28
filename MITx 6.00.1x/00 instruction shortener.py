426d4a755e1f9e09ef36c400f37e7549dd9f14d6
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-06-28 00:11:38 -0400

    completed next 2 assignments

--- MITx 6.00.1x/00 instruction shortener.py
+++ MITx 6.00.1x/00 instruction shortener.py
@@ -11,23 +11,55 @@ TO DO:
 Idea for v2: interact with other .py files
 '''
 ##############################################################
-
 # I'm thinking it's easiest to have the big string field here
 instructions = '''
 
+Next, implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
+
+Example Usage:
+
+>>> secretWord = 'apple' 
+>>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+>>> print(getGuessedWord(secretWord, lettersGuessed))
+'_ pp_ e'
+When inserting underscores into your string, it's a good idea to add at least a space after each one, so it's clear to the user how many unguessed letters are left in the string (compare the readability of ____ with _ _ _ _ ). This is called usability - it's very important, when programming, to consider the usability of your program. If users find your program difficult to understand or operate, they won't use it!
+
+For this problem, you are free to use spacing in any way you wish - our grader will only check that the letters and underscores are in the proper order; it will not look at spacing. We do encourage you to think about usability when designing.
+
+For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
 
 
-Hints:
-You should start by noticing where we're using the provided functions (at the top of ps3_hangman.py) to load the words and pick a random one. Note that the functions loadWords and chooseWord should only be used on your local machine,
 
 '''
 
 
 
 ##############################################################
-debug = True
+debug = False
+
+# example to keep that actually broke something
+if debug == True:
+    instructions = '''
+    
+    Next, implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
+
+    Example Usage:
+
+    >>> secretWord = 'apple' 
+    >>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+    >>> print(getGuessedWord(secretWord, lettersGuessed))
+    '_ pp_ e'
+    When inserting underscores into your string, it's a good idea to add at least a space after each one, so it's clear to the user how many unguessed letters are left in the string (compare the readability of ____ with _ _ _ _ ). This is called usability - it's very important, when programming, to consider the usability of your program. If users find your program difficult to understand or operate, they won't use it!
+
+    For this problem, you are free to use spacing in any way you wish - our grader will only check that the letters and underscores are in the proper order; it will not look at spacing. We do encourage you to think about usability when designing.
+
+    For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
+
+    
+    '''
+
 # max length of string before break.
-max_len = 79
+max_len = 78
 if debug:
     max_len = 20 # to make debugging easier
 last_space, new_line_tracker = 0, 0
