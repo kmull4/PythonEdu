d7ede5fd57b9adc16eb2deb15c109f475d61b1f1
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-07-07 17:03:06 -0400

    add logging ability & works now
    changed from printing out debug logs in the console to writing a log file (the first log files I've ever programmed actually!), separated many processes into functions, and adjusted new_line_tracker to work properly. It also works now, provided that the max length is greater than the largest word.

--- MITx 6.00.1x/01 instruction shortener experimenting.py
+++ MITx 6.00.1x/01 instruction shortener experimenting.py
@@ -13,101 +13,97 @@ Idea for v2: interact with other .py files
 ##############################################################
 # I'm thinking it's easiest to have the big string field here
 instructions = '''
-
 Next, implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
 
 Example Usage:
-
->>> secretWord = 'apple' 
->>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
->>> print(getGuessedWord(secretWord, lettersGuessed))
-'_ pp_ e'
 When inserting underscores into your string, it's a good idea to add at least a space after each one, so it's clear to the user how many unguessed letters are left in the string (compare the readability of ____ with _ _ _ _ ). This is called usability - it's very important, when programming, to consider the usability of your program. If users find your program difficult to understand or operate, they won't use it!
 
 For this problem, you are free to use spacing in any way you wish - our grader will only check that the letters and underscores are in the proper order; it will not look at spacing. We do encourage you to think about usability when designing.
-
 For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
-
-
 
 '''
 
 
 
 ##############################################################
+# variables that need initializing
+last_space, new_line_tracker, max_len = 0, 0, 78
 debug = True
-
 # example to keep that actually broke something
 if debug == True:
-    instructions = '''
-
-Next, implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
+    max_len = 20 # to make debugging easier
+    instructions = '''Next, implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
 
 Example Usage:
 When inserting underscores into your string, it's a good idea to add at least a space after each one, so it's clear to the user how many unguessed letters are left in the string (compare the readability of ____ with _ _ _ _ ). This is called usability - it's very important, when programming, to consider the usability of your program. If users find your program difficult to understand or operate, they won't use it!
 
 For this problem, you are free to use spacing in any way you wish - our grader will only check that the letters and underscores are in the proper order; it will not look at spacing. We do encourage you to think about usability when designing.
-For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
+For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.'''
 
-
-'''
-# max length of string before break.
-max_len = 78
-if debug:
-    max_len = 20 # to make debugging easier
-last_space, new_line_tracker = 0, 0
-
-
-'''         ~~make this into a function:~~
-elif (position - new_line_tracker) % max_len == 0 and position != 0:
-    # this replaces the last space with a newline
-    instructions = instructions[:last_space] + str('\n') + \
-        instructions[last_space+1:]
-    # it's very ironic that this^ line goes over 80 characters
-'''
-def new_line_inserter(position, new_line_tracker):
-    global instructions
-    if (position - new_line_tracker) % max_len == 0 and position != 0:
-        instructions = instructions[:last_space] + str('\n') + \
-            instructions[last_space+1:]
-        # it's very ironic that this^ line goes over 80 characters
+# functions~
+def append_log(mystr):
+    # log text file. easier to read than console output.
+    # takes in a string, adds string to the log
+    mystr = str(mystr)
+    f.write(mystr)
     return
 
-if debug: # a good starting point
-    print(instructions)
-    print(len(instructions), 'characters total, breaking at', max_len, '\n')
-    
+def is_new_line_needed():
+    # determines if new line is needed or not
+    if position == 0 or new_line_tracker == 0:
+        return False
+    elif new_line_tracker % max_len == 0:
+        return True
+    else:
+        return False
+
+def new_line_inserter():
+    global instructions, new_line_tracker
+    instructions = instructions[:last_space] + str('\n') + \
+        instructions[last_space+1:]
+        # it's rather ironic that this^ line goes over 80 characters
+    new_line_tracker = position - last_space
+    return
+
+# clear previous log
+f = open('01_log.txt', "w") # this saves in \users\me by default
+f.write("")
+f.close()
+# now write a new one
+f = open('01_log.txt', "a")
+# a good starting point to log
+append_log(instructions)
+append_log('\n' + str(len(instructions)) + ' characters total, ')
+append_log('breaking at ' + str(max_len) + '\n~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
+
 # use enumerate method to keep track of where we are
 for position, letter in enumerate(instructions):
-    if debug:
-        print(position, letter, end=", ") # where are we, really?
+    append_log(str(position) +' '+ str(letter) + \
+               ', ') # where are we, really?
     # take note if the character is a space
     if letter == ' ':
         last_space = position
-        if debug:
-            print('last_space =', last_space)
-    # reset counter if there's a new line
+        append_log('\nlast_space = ' + str(last_space) + '\n')
+    
+    # reset counters and skip new line checker if there's a new line
     if letter == '\n':
-        new_line_tracker = position
-        if debug:
-            print('New line detected, position at', position)
-    # this determines if we've hit the line length
-    # done as an elif because a new line is already there
-    elif (position - new_line_tracker) % max_len == 0 and position != 0:
-        # this replaces the last space with a newline
-        instructions = instructions[:last_space] + str('\n') + \
-            instructions[last_space+1:]
-        # it's rather ironic that this^ line goes over 80 characters
-        
-        if debug:
-            print('newline inserted at last space. position was', position, \
-                      'last space =', last_space)
-            print(instructions)
-        
+        last_space = position
+        new_line_tracker = -1 # because it adds it back soon
+        append_log('New line detected, position at ' + str(position) +'\n')
+    # if it is a new line already, don't check for new line
+    elif is_new_line_needed():
+        new_line_inserter()
+        append_log('newline inserted at last space. position now ' + \
+                   str(position) + ', last space = ' + str(last_space) + \
+                       ', new_line_tracker = ' + str(new_line_tracker))
+        append_log('\n~~vv~~\n' + str(instructions) + '\n~~^^~~\n\n')
+    new_line_tracker += 1
 
-# print the results to copy from the console
-if debug:
-    print('\n\nfinal:')
-    print(instructions)
-else:
-    print(instructions)
+# log the final values
+append_log('\n\nfinal:\n' + instructions)
+
+# print in the console to copy and use
+print(instructions)
+
+# close the log
+f.close()
