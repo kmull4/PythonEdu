92e6432e9a843910859e3b0fd613256401e599ae
kmull4 <103075134+kmull4@users.noreply.github.com>
2022-10-17 22:53:45 -0400

    Create zero_one_knapsacks_trinary.py
    This took some abstract thinking but it was really cool when I figured it out.

---
+++ MITx 6002x/zero_one_knapsacks_trinary.py
@@ -0,0 +1,34 @@
+items = ['a', 'b', 'c']
+
+# generate all combinations of N items
+def powerSet(items):
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1: # ">>" = i // 2**j
+                combo.append(items[j])
+        yield combo
+
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
+    N = len(items)
+    # enumerate the 3**N possible combinations
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i // 3**j) % 3 == 1: # ">>" = i // 2**j
+                bag2.append(items[j])
+            elif (i // 3**j) % 3 == 2: # ">>" = i // 2**j
+                bag1.append(items[j])
+        yield (bag1, bag2)
